426e34cf98057b67599e0f37e7dc6b462a220272
matisse.tsang <matisse.tsang@hunghingprinting.com>
2025-06-13 11:26:30 +0800

    test

--- hh_chatbot/util/chatbot_util.py
+++ hh_chatbot/util/chatbot_util.py
@@ -8,6 +8,9 @@ from dotenv import load_dotenv
 script_dir = os.path.dirname(__file__)
 env_path = os.path.join(script_dir, '.env')
 
+def test():
+    print("test")
+
 def timeout(seconds=10, error_message='Function call timed out'):
     def decorator(func):
         @wraps(func)
